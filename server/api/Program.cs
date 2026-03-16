781e8bfbd68b4286ee4f7d4d31875c35c050f079
Henrik Jessen Munch <henrikjessenmunch@gmail.com>
2026-03-16 18:10:02 +0100

    Change efscaffold namespace

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using efscaffold;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
