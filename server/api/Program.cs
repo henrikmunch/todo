e8d1b2093b915293805a808bfee40686fe4f8921
Henrik Jessen Munch <henrikjessenmunch@gmail.com>
2026-03-17 16:32:21 +0100

    Update gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,15 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using efscaffold;
+using efscaffold.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql();
+    conf.UseNpgsql("Server=ep-nameless-bonus-aha0bgj9-pooler.c-3.us-east-1.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_KB7xHRfntOF4;SslMode=require");
 });
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (MyDbContext context) =>
+{
+    var Todo = new Todo()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Title = "Title",
+        Description = "Description",
+        Isdone = false,
+        Priority = 5,
+    };
+
+    context.Todos.Add(Todo);
+    context.SaveChanges();
+
+    var objects = context.Todos.ToList();
+
+    return objects;
+});
 
 app.Run();
